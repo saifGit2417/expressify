5b942de7f80b58a333b4a6a7ece57440815e82a7
Md Saif Ur Rahman <saifwork2417@gmail.com>
2024-04-28 19:24:13 +0530

    feat : delete to do functionality

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -8,9 +8,10 @@ import {
 
 const toDoRouter = express.Router();
 const prisma = new PrismaClient();
+toDoRouter.use(authMiddleWare);
 
 // http://localhost:3333/api/v1/todo/getAllToDos/3
-toDoRouter.get("/getAllToDos/:userId", authMiddleWare, async (req, res) => {
+toDoRouter.get("/getAllToDos/:userId", async (req, res) => {
   try {
     const userId = parseInt(req.params.userId);
     const getAllToDos = await prisma.toDos.findMany({
@@ -26,7 +27,7 @@ toDoRouter.get("/getAllToDos/:userId", authMiddleWare, async (req, res) => {
   }
 });
 
-toDoRouter.get("/getAllToDos", authMiddleWare, async (req, res) => {
+toDoRouter.get("/getAllToDos", async (req, res) => {
   try {
     const getAllToDos = await prisma.toDos.findMany();
     res.status(200).json({
@@ -37,7 +38,7 @@ toDoRouter.get("/getAllToDos", authMiddleWare, async (req, res) => {
   }
 });
 
-toDoRouter.post("/add", authMiddleWare, addToDoValidate, async (req, res) => {
+toDoRouter.post("/add", addToDoValidate, async (req, res) => {
   try {
     const { userId } = req.headers;
     const { title, description } = req.body;
@@ -62,29 +63,43 @@ toDoRouter.post("/add", authMiddleWare, addToDoValidate, async (req, res) => {
 http://localhost:3333/api/v1/todo/editToDo/5
 body requires
 */
-toDoRouter.put(
-  "/editToDo/:toDoId",
-  updateToDoValidate,
-  authMiddleWare,
-  async (req, res) => {
-    try {
-      const toDoId = parseInt(req.params.toDoId);
-      const { title, description } = req.body;
-      const updateToDo = await prisma.toDos.update({
-        where: {
-          id: toDoId,
-        },
-        data: {
-          title,
-          description,
-        },
+toDoRouter.put("/editToDo/:toDoId", updateToDoValidate, async (req, res) => {
+  try {
+    const toDoId = parseInt(req.params.toDoId);
+    const { title, description } = req.body;
+    const updateToDo = await prisma.toDos.update({
+      where: {
+        id: toDoId,
+      },
+      data: {
+        title,
+        description,
+      },
+    });
+    if (updateToDo) {
+      res.status(200).json({
+        message: "to do updated successfully",
       });
-      if (updateToDo) {
-        res.status(200).json({
-          message: "to do updated successfully",
-        });
-      }
-    } catch (error) {}
+    }
+  } catch (error) {}
+});
+
+// http://localhost:3333/api/v1/todo/delete/4
+toDoRouter.delete("/delete/:toDoId", async (req, res) => {
+  try {
+    const toDoId = parseInt(req.params.toDoId);
+    const deleteToDo = await prisma.toDos.delete({
+      where: {
+        id: toDoId,
+      },
+    });
+    if (deleteToDo) {
+      res.status(200).json({
+        message: "to do deleted successfully",
+      });
+    }
+  } catch (error) {
+    console.log("error: ", error);
   }
-);
+});
 export default toDoRouter;
